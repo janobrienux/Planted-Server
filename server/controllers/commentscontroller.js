e019a6e788ba35e4f4f78e6e81f01fec9ad02e0e
January <januaryobrien.ux@gmail.com>
2020-12-30 14:52:50 -0500

    updated commet controller and added three new endpoints

--- server/controllers/commentscontroller.js
+++ server/controllers/commentscontroller.js
@@ -3,6 +3,9 @@ const validateSession = require('../middleware/validateSession');
 const  Comments  = require('../models/comments');
 const router = Router();
 
+
+
+// CREATE COMMENTS
 router.post('/create', validateSession, async (req, res) => {
   
   try {
@@ -19,7 +22,45 @@ router.post('/create', validateSession, async (req, res) => {
         message: 'Comment failed'
     })
 }
-})
+});
+
+
+//GET COMMENTS BY USER 
+router.get("/mine", validateSession, (req, res) => {
+  let userid = req.user.id
+  Comment.findAll ({
+      where: { userId: userId}
+  })
+      .then(comments => res.status(200).json(comments))
+      .catch(err => res.status(500).json({ error: error }))
+});
+
+
+//COMMENTS UPDATE 
+router.put('/update/:entryId', validateSession, function (req, res) {
+  const updateCommentEntry = {
+      userName: req.body.comment.userName,
+      date: req.body.comment.date,
+      entry: req.body.comment.entry,
+  };
+
+  const query = { where: { id: req.params.entryId, owner: req.user.id, }};
+
+  Comment.update(updateCommentEntry, query)
+      .then(() => res.status(200).json({message: 'Comment has been updated!'}))
+      .catch((error) => res.status(500).json({ error: error.message || serverErrorMsg  }));
+});
+
+// DELETE COMMENTS
+
+router.delete('/delete/:id', validateSession, function (req, res) {
+  const query = { where: {id: req.params.id, owner: req.user.id }};
+
+  Comment.destroy(query)
+      .then(() => res.status(200).json({ message: 'Comment has been deleted!'}))
+      .catch((err) => res.status(500).json ({ error: err }));
+});
+
 
 
 module.exports = router;
